9973c9e7311040d80e7d104422277bb6a9be73bc
Nicole Mazour <nmazour@nebrwesleyan.edu>
2014-11-04 09:39:25 -0500

    Completed Worksheet

--- index.js
+++ index.js
@@ -75,3 +75,124 @@ function sum(numbers) {
   }
   return total;
 }
+
+function four_of_a_kind(five_dice_rolls) {
+  var score;
+  score = 0;
+  if (five_dice_rolls[0] === five_dice_rolls[1]) {
+    if (five_dice_rolls[1] === five_dice_rolls[2]) {
+      if (five_dice_rolls[2] === five_dice_rolls[3]) {
+        score = sum(five_dice_rolls);
+      }
+    }
+  }
+  if (five_dice_rolls[1] === five_dice_rolls[2]) {
+    if (five_dice_rolls[2] === five_dice_rolls[3]) {
+      if (five_dice_rolls[3] === five_dice_rolls[4]) {
+        score = sum(five_dice_rolls);
+      }
+    }
+  }
+  if (five_dice_rolls[0] === five_dice_rolls[2]) {
+    if (five_dice_rolls[2] === five_dice_rolls[3]) {
+      if (five_dice_rolls[3] === five_dice_rolls[4]) {
+        score = sum(five_dice_rolls);
+      }
+    }
+  }
+  if (five_dice_rolls[0] === five_dice_rolls[1]) {
+    if (five_dice_rolls[1] === five_dice_rolls[2]) {
+      if (five_dice_rolls[2] === five_dice_rolls[4]) {
+        score = sum(five_dice_rolls);
+      }
+    }
+  }
+  if (five_dice_rolls[0] === five_dice_rolls[1]) {
+    if (five_dice_rolls[1] === five_dice_rolls[3]) {
+      if (five_dice_rolls[3] === five_dice_rolls[4]) {
+        score = sum(five_dice_rolls);
+      }
+    }
+  }
+  return score;
+}
+
+function large_straight(five_dice_rolls) {
+  var score;
+  score = 0;
+  five_dice_rolls = five_dice_rolls.sort();
+  if (five_dice_rolls[0] === five_dice_rolls[1] - 1) {
+    if (five_dice_rolls[1] === five_dice_rolls[2] - 1) {
+      if (five_dice_rolls[2] === five_dice_rolls[3] - 1) {
+        if (five_dice_rolls[3] === five_dice_rolls[4] - 1) {
+        score = 40;
+        }
+      }
+    }
+  }
+  return score;
+}
+
+function full_house(five_dice_rolls) {
+  var score;
+  score = 0;
+  five_dice_rolls = five_dice_rolls.sort();
+  if (five_dice_rolls[0] === five_dice_rolls[1]) {
+    if (five_dice_rolls[1] === five_dice_rolls[2]) {
+      if (five_dice_rolls[3] === five_dice_rolls[4]) {
+        score = 25;
+      }
+    }
+  }
+  if (five_dice_rolls[0] === five_dice_rolls[1]) {
+    if (five_dice_rolls[2] === five_dice_rolls[3]) {
+      if (five_dice_rolls[3] === five_dice_rolls[4]) {
+        score = 25;
+      }
+    }
+  }
+  return score;
+}
+
+function Yahtzee_scores(functions, five_dice_rolls) {
+  var scores;
+  scores = [];
+  for (var roll = 0; scores.length < functions.length; roll = roll +1) {
+    scores[roll] = functions[roll](five_dice_rolls);
+  }
+  return scores;
+}
+
+function maximum(numbers) {
+  var max;
+  max = -Infinity;
+  for (var current = 0; current < numbers.length; current = current +1) {
+    if (numbers[current] > max) {
+      max = numbers[current];
+    }
+  }
+  return max;
+}
+
+function highest_score(five_dice_rolls) {
+  var score;
+  score = 0;
+  if (Yahtzee(five_dice_rolls) === 50) {
+    score = 50;
+  }
+  if (large_straight(five_dice_rolls) === 40) {
+    score = 40;
+  }
+  if (four_of_a_kind(five_dice_rolls) === sum(five_dice_rolls)) {
+    if (score < sum(five_dice_rolls)) {
+      score = sum(five_dice_rolls);
+    }
+  }
+  return score;
+}
+
+function number_of_rolls() {
+  for (var rolls = 0; highest_score(five_dice_rolls()) < 25; rolls = rolls + 1) {
+  }
+  return rolls;
+}
